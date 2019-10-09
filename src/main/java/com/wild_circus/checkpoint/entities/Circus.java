f30e5178762b01b65e7cd481974f8773fbe515f8
Wilder <n.huraux@hotmail.com>
2019-10-09 14:47:14 +0200

    page login focntionne

--- src/main/java/com/wild_circus/checkpoint/entities/Circus.java
+++ src/main/java/com/wild_circus/checkpoint/entities/Circus.java
@@ -1,8 +1,58 @@
 package com.wild_circus.checkpoint.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Circus {
-    
+    public Circus(){
+    }
+
+    public Circus(String nom, String ville, int note) {
+        this.nom = nom;
+        this.ville = ville;
+        this.note = note;
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String ville;
+    private int note;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    public void setNote(int note) {
+        this.note = note;
+    }
 }
